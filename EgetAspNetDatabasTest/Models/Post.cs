407cc80e12c0bf1718dfa8c13a331d34a9188724
Ludvig Andersson <mlt.andersson@outlook.com>
2024-02-26 09:51:34 +0100

    Update Post.cs

--- EgetAspNetDatabasTest/Models/Post.cs
+++ EgetAspNetDatabasTest/Models/Post.cs
@@ -8,11 +8,11 @@ namespace EgetAspNetDatabasTest.Models
         public int PostId { get; set; } // Primärnyckel
         public string PostData { get; set; }
         [ForeignKey("User")]
-        public int UserId { get; set; } // Främmande nyckel
+        public int UserId { get; set; } // Foreign nyckel
         public int PostDownvotes { get; set; }
         public int PostUpvotes { get; set; }
 
-        // Navigationsegenskap: Varje inlägg är kopplat till en användare
+        // Varje inlägg är kopplat till en användare
         public User User { get; set; }
     }
 }
